9ee210ad16a40942e1c3e6788a8c85f796e87f9e
Yash Sakle <yash.sakle23@gmail.com>
2023-01-10 15:54:25 +0530

    feat: can upload photos

--- src/main/java/com/jetbrains/yash/Photoclone/PhotoController.java
+++ src/main/java/com/jetbrains/yash/Photoclone/PhotoController.java
@@ -3,8 +3,10 @@ package com.jetbrains.yash.Photoclone;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -34,9 +36,12 @@ public class PhotoController {
         Photo photo = db.remove(id);
         if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
-    @PostMapping("/photos/")
-    public Photo create(@RequestBody @Valid Photo photo){
+    @PostMapping("/photos")
+    public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
+        Photo photo = new Photo();
         photo.setId(UUID.randomUUID().toString());
+        photo.setFileName(file.getOriginalFilename());
+        photo.setData(file.getBytes());
         db.put(photo.getId(), photo);
         return photo;
     }
